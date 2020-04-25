9588ae6ddef9e90d110fdf61e3e0832c35ad773d
Paolo Pastacaldi <ppastacaldi@outlook.it>
2020-04-25 16:43:45 +0200

    first and last commit

---
+++ main.js
@@ -0,0 +1,37 @@
+/* changing class on hover */
+
+const card = document.querySelectorAll('.card');
+card.forEach((card) => {
+    card.addEventListener('mouseover', (event) => {
+        card.classList.add("card-over")
+        card.addEventListener('mouseout', (e) => {
+            card.classList.remove("card-over");
+        });
+    });
+});
+
+/* switch annual-monthly */
+
+const toggle = document.querySelector("input");
+const annual = document.querySelectorAll("#year")
+const monthly = document.querySelectorAll("#month")
+
+toggle.addEventListener("change", (e) => {
+    if (toggle.checked) {
+        annual.forEach((a) => {
+            a.style.display = "none"
+        })
+        monthly.forEach((m) => {
+
+            m.style.display = "block";
+        })
+    } else {
+        annual.forEach((a) => {
+            a.style.display = ""
+        })
+        monthly.forEach((m) => {
+
+            m.style.display = "none";
+        })
+    }
+})
