e5b5bfbd82a0e5a1887710d9b336332bee01d0f6
Paolo Pastacaldi <ppastacaldi@outlook.it>
2020-04-27 10:59:13 +0200

    refactoring

--- main.js
+++ main.js
@@ -1,37 +1,21 @@
-/* changing class on hover */
-
-const card = document.querySelectorAll('.card');
-card.forEach((card) => {
-    card.addEventListener('mouseover', (event) => {
-        card.classList.add("card-over")
-        card.addEventListener('mouseout', (e) => {
-            card.classList.remove("card-over");
-        });
-    });
-});
-
 /* switch annual-monthly */
 
 const toggle = document.querySelector("input");
 const annual = document.querySelectorAll(".year")
 const monthly = document.querySelectorAll(".month")
 
+
+const toggleDisplay = value => el => {
+    el.style.display = value
+}
+
+
 toggle.addEventListener("change", (e) => {
-    if (toggle.checked) {
-        annual.forEach((a) => {
-            a.style.display = "none"
-        })
-        monthly.forEach((m) => {
-
-            m.style.display = "block";
-        })
+    if (e.target.checked) {
+        annual.forEach(toggleDisplay("none"))
+        monthly.forEach(toggleDisplay("block"))
     } else {
-        annual.forEach((a) => {
-            a.style.display = ""
-        })
-        monthly.forEach((m) => {
-
-            m.style.display = "none";
-        })
+        annual.forEach(toggleDisplay("block"))
+        monthly.forEach(toggleDisplay("none"))
     }
 })
